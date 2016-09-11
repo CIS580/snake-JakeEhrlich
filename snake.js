8a4e0d4125cfcab598269f378ce309fd6acb3277
Jake Ehrlich <jake.h.ehrlich@gmail.com>
2016-09-11 22:46:12 +0000

    finished the game

--- snake.js
+++ snake.js
@@ -1,4 +1,6 @@
-/* Global variables */
+/*///-----------------------------------------------------------------------
+| Global varibles used mainly in rendering stuff
+*///------------------------------------------------------------------------
 var frontBuffer = document.getElementById('snake');
 var frontCtx = frontBuffer.getContext('2d');
 var backBuffer = document.createElement('canvas');
@@ -6,10 +8,10 @@ backBuffer.width = frontBuffer.width;
 backBuffer.height = frontBuffer.height;
 var backCtx = backBuffer.getContext('2d');
 var oldTime = performance.now();
-//var gridWidth = Math.floor(frontBuffer.width / scale);
-//var gridHeight = Math.floor(frontBuffer.height / scale);
 
-/* Helper functions */
+/*///-----------------------------------------------------------------------
+| A bunch of helper functions
+*///------------------------------------------------------------------------
 
 //converts rectangular corrdinates to polar
 function toPolar(x, y) {
@@ -18,6 +20,7 @@ function toPolar(x, y) {
 
 //tweens a value a specified ratio between two values
 function tween(start, finish, by, mn, mx) {
+  //neat trick from stack overflow that I use a lot in this program
   //http://stackoverflow.com/questions/16201656/how-to-swap-two-variables-in-javascript
   if(mx < mn) mn = [mx, mx = mn][0];
   var diff = mx - mn
@@ -29,6 +32,7 @@ function gridSnap(pos, scale) {
   return [Math.floor(pos[0] / scale), Math.floor(pos[1] / scale)];
 }
 
+//apprently javascript has no way to compare arrays for equality so I got this from here
 //http://stackoverflow.com/questions/3115982/how-to-check-if-two-arrays-are-equal-with-javascript
 function arraysEqual(a, b) {
   if (a === b) return true;
@@ -92,9 +96,25 @@ function mag(v) {
   return Math.sqrt(out);
 }
 
+function normalize(v) {
+  return smul(1/mag(v), v);
+}
+
 function eqv(v1, v2) { return arraysEqual(v1, v2); }
 
-/* Snake Game class: acts more or less as the model*/
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// Returns a random integer between min (included) and max (excluded)
+// Using Math.round() will give you a non-uniform distribution!
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+/*///-----------------------------------------------------------------------
+| A class to define the varibles of a snake game and change them
+*///------------------------------------------------------------------------
+
 function SnakeGame(speed, growth, scale, gridWidth, gridHeight) {
   this.dir = [1, 0];
   this.turn = [];
@@ -110,29 +130,42 @@ function SnakeGame(speed, growth, scale, gridWidth, gridHeight) {
   this.apples = {}; //dictionary to lookup and set apples
   this.theta = 0;
 }
-SnakeGame.prototype.addApple = function(x, y) {
-  this.apples[[x, y]] = true;
+SnakeGame.prototype.addApple = function(pos) {
+  this.apples[pos] = true;
 }
-SnakeGame.prototype.removeApple = function(x, y) {
-  delete apples[[x, y]];
+SnakeGame.prototype.removeApple = function(pos) {
+  console.log(this.apples);
+  delete this.apples[pos];
+  console.log(this.apples);
+  console.log(Object.keys(this.apples));
 }
-SnakeGame.prototype.isAppleAt = function(x, y){ 
-  return !!this.apples[[x, y]];
+SnakeGame.prototype.getApples = function() {
+  console.log(this.apples);
+  return Object.keys(this.apples).map(function(p){ return JSON.parse("["+p+"]")})
+}
+SnakeGame.prototype.isAppleAt = function(pos){ 
+  return !!this.apples[pos];
 }
 SnakeGame.prototype.checkSelfCollide = function() {
-  for(var i = 0; i < this.turnPoints.length - 1; ++i) {
-    var p1 = gridSnap(this.turnPoints[i], this.scale);
-    var p2 = gridSnap(this.turnPoints[i + 1], this.scale);
-    var gp = this.gridPos();
-    var idx = +(p1[0] == p2[0]);
-    var hit = Math.min(p1[idx], p2[idx]) <= gp[idx] && gp[idx] >= Math.max(p1[idx], p2[idx]);
-    if(hit) return true;
+  var visited = {};
+  var nodes = [this.tail].concat(this.turnPoints.concat([this.head]));
+  var self=this;
+  nodes = nodes.map(function(p){return gridSnap(p, self.scale);});
+  for(var i = 0; i < nodes.length - 1; ++i) {
+    var p1 = nodes[i];
+    var p2 = nodes[i+1];
+    var diff = normalize(subv(p2, p1)); //should be [0,1],[0,-1],[1,0], or [-1,0]
+    while(!eqv(p1, p2)) {
+      if(visited[p1]) return true;
+      visited[p1] = true;
+      p1 = addv(p1, diff);
+    }
   }
   return false;
 }
 SnakeGame.prototype.checkOutOfBounds = function() {
   var gp = this.gridPos();
-  return gp[0] <= this.gridW && gp[1] <= this.gridH;
+  return gp[0] < 0 || gp[1] < 0 || gp[0] >= this.gridW || gp[1] >= this.gridH;
 }
 SnakeGame.prototype.gridPos = function() {
   return gridSnap(this.head, this.scale);
@@ -186,62 +219,9 @@ SnakeGame.prototype.move = function(dt) {
   }
 }
 
-//
-
-/* now we setup the game details */
-var snakeGameScale = 20;
-snakeGame = new SnakeGame(0.1, 0.01, snakeGameScale, 
-                         frontBuffer.width/snakeGameScale, frontBuffer.height/snakeGameScale)
-
-document.onkeydown = function(e) {
-  e = e || window.event;
-  switch(e.code) {
-    case 'ArrowUp':    snakeGame.addTurn([0, -1]); break;
-    case 'ArrowDown':  snakeGame.addTurn([0,  1]); break;
-    case 'ArrowRight': snakeGame.addTurn([1,  0]); break;
-    case 'ArrowLeft':  snakeGame.addTurn([-1, 0]); break;
-    case 'Space': snakeGame.speed = 0.0; snakeGame.tailspeed = 0.0; break;
-  }
-  console.log(e);
-}
-
-/**
- * @function loop
- * The main game loop.
- * @param{time} the current time as a DOMHighResTimeStamp
- */
-function loop(newTime) {
-  var elapsedTime = newTime - oldTime;
-  oldTime = newTime;
-
-  update(elapsedTime);
-  frontCtx.clearRect(0, 0, backBuffer.width, backBuffer.height);
-  render(elapsedTime);
-  frontCtx.drawImage(backBuffer, 0, 0);
-
-  // Run the next loop
-  window.requestAnimationFrame(loop);
-}
-
-/**
- * @function update
- * Updates the game state, moving
- * game objects and handling interactions
- * between them.
- * @param {elapsedTime} A DOMHighResTimeStamp indicting
- * the number of milliseconds passed since the last frame.
- */
-function update(elapsedTime) {
-  snakeGame.move(elapsedTime);
-  // TODO: Spawn an apple periodically
-  // TODO: Grow the snake periodically
-  // TODO: Move the snake
-  // TODO: Determine if the snake has moved out-of-bounds (offscreen)
-  // TODO: Determine if the snake has eaten an apple
-  // TODO: Determine if the snake has eaten its tail
-  // TODO: [Extra Credit] Determine if the snake has run into an obstacle
-
-}
+/*///-----------------------------------------------------------------------
+| A bunch of functions to help render stuff
+*///------------------------------------------------------------------------
 
 //used for debugging of the visual effects and movement
 function renderGrid(ctx, gridW, gridH, scale) {
@@ -329,6 +309,93 @@ function radialSweep(ctx, res, line1, line2, xoff, yoff) {
 }
 
 
+/*///-----------------------------------------------------------------------
+| Game Definition
+|    now we define all the little speicifcs
+*///------------------------------------------------------------------------
+
+
+/* now we setup the game details */
+var score = 0;
+var snakeGameScale = 20;
+snakeGame = new SnakeGame(0.1, 0.01, snakeGameScale, 
+                         frontBuffer.width/snakeGameScale, frontBuffer.height/snakeGameScale)
+
+//add an apple before the games starts
+var x = getRandomInt(3,snakeGame.gridW-3);
+var y = getRandomInt(3,snakeGame.gridH-3);
+snakeGame.addApple([x,y]);
+
+document.onkeydown = function(e) {
+  e = e || window.event;
+  switch(e.code) {
+    case 'ArrowUp':    snakeGame.addTurn([0, -1]); break;
+    case 'ArrowDown':  snakeGame.addTurn([0,  1]); break;
+    case 'ArrowRight': snakeGame.addTurn([1,  0]); break;
+    case 'ArrowLeft':  snakeGame.addTurn([-1, 0]); break;
+    case 'Space': snakeGame.speed = 0.0; snakeGame.tailspeed = 0.0; break;
+  }
+  console.log(e);
+}
+
+/**
+ * @function loop
+ * The main game loop.
+ * @param{time} the current time as a DOMHighResTimeStamp
+ */
+function loop(newTime) {
+  var elapsedTime = newTime - oldTime;
+  oldTime = newTime;
+
+  if(update(elapsedTime)) return window.requestAnimationFrame(looseLoop);
+  frontCtx.clearRect(0, 0, backBuffer.width, backBuffer.height);
+  render(elapsedTime);
+  frontCtx.drawImage(backBuffer, 0, 0);
+
+  // Run the next loop
+  window.requestAnimationFrame(loop);
+}
+
+function looseLoop(_) {
+  backCtx.font="48px Hellki";
+  backCtx.fillText("You Dead",backBuffer.width/2-140,backBuffer.height/2);
+  frontCtx.clearRect(0, 0, backBuffer.width, backBuffer.height);
+  frontCtx.drawImage(backBuffer, 0, 0);
+  window.requestAnimationFrame(looseLoop);
+}
+
+/**
+ * @function update
+ * Updates the game state, moving
+ * game objects and handling interactions
+ * between them.
+ * @param {elapsedTime} A DOMHighResTimeStamp indicting
+ * the number of milliseconds passed since the last frame.
+ */
+function update(elapsedTime) {
+  snakeGame.move(elapsedTime);
+  if(snakeGame.checkSelfCollide()) {
+    //TODO: lose/restart game
+    console.log("hit!");
+    return true;
+  }
+  if(snakeGame.isAppleAt(snakeGame.gridPos())) {
+    console.log("you ate an apple!");
+    //TODO: update points
+    snakeGame.removeApple(snakeGame.gridPos());
+    var x = getRandomInt(0,snakeGame.gridW);
+    var y = getRandomInt(0,snakeGame.gridH);
+    snakeGame.addApple([x,y]);
+    snakeGame.speed *= 1.02;
+    score = score + 1;
+  }
+  if(snakeGame.checkOutOfBounds()) {
+    //TODO: lose/restart game
+    console.log("out of bounds!");
+    return true;
+  }
+  return false;
+}
 
 /**
   * @function render
@@ -338,31 +405,50 @@ function radialSweep(ctx, res, line1, line2, xoff, yoff) {
   */
 function render(elapsedTime) {
   backCtx.clearRect(0, 0, backBuffer.width, backBuffer.height);
-  renderGrid(backCtx, snakeGame.gridW, snakeGame.gridH, snakeGame.scale);
+  //renderGrid(backCtx, snakeGame.gridW, snakeGame.gridH, snakeGame.scale);
   renderCircle(backCtx, snakeGame.scale/2, snakeGame.head[0], snakeGame.head[1]);
   renderCircle(backCtx, snakeGame.scale/2, snakeGame.tail[0], snakeGame.tail[1]);
   var snakeRadius = 3;
   var points = [snakeGame.tail].concat(snakeGame.turnPoints.concat([snakeGame.head]));
+  var off = snakeGame.scale/2;
+  function wave(amp, lam, vol, x, t) {
+    var factor = Math.PI*2/lam;
+    return amp*Math.sin(factor*x - factor*vol*t);
+  }
+  var wl = 40;
+  var vol = 10/1000;
+  var d = new Date();
+  var n = d.getTime();
   for(var i = 0; i < points.length - 1; ++i) {
     var p1 = points[i];
     var p2 = points[i+1];
     var delta = subv(p1, p2);
-    //if(delta[0] == 0) {
-      renderVertPath(backCtx, 
-                     function(y) {return 3*Math.cos(y/10) + p1[0] - snakeRadius;}, 
-                     function(y) {return 3*Math.cos(y/10) + p1[0] + snakeRadius;},
-                     Math.floor(Math.abs(p1[1] - p2[1])), p1[1], p2[1]);
-    //} else {
-      renderHorzPath(backCtx, 
-                     function(x) {return 3*Math.cos(x/10) + p2[1] - snakeRadius;}, 
-                     function(x) {return 3*Math.cos(x/10) + p2[1] + snakeRadius;},
-                     Math.floor(Math.abs(p1[0] - p2[0])), p1[0], p2[0]);
-    //}
-                     
+    var mny = Math.min(p1[1], p2[1]);
+    var mnx = Math.min(p1[0], p2[0]);
+    var mxy = Math.max(p1[1], p2[1]);
+    var mxx = Math.max(p1[0], p2[0]);
+    renderVertPath(backCtx, 
+                   function(y) {return wave(3,wl,vol,y,n) + p1[0] - snakeRadius;}, 
+                   function(y) {return wave(3,wl,vol,y,n) + p1[0] + snakeRadius;},
+                   Math.floor(Math.abs(p1[1] - p2[1])), mny + off, mxy - off);
+    renderHorzPath(backCtx, 
+                   function(x) {return wave(3,wl,vol,x,n) + p2[1] - snakeRadius;}, 
+                   function(x) {return wave(3,wl,vol,x,n) + p2[1] + snakeRadius;},
+                   Math.floor(Math.abs(p1[0] - p2[0])), mnx + off, mxx - off);       
   }
   for(var i = 0; i < snakeGame.turnPoints.length; ++i) {
     renderCircle(backCtx, snakeGame.scale/2, snakeGame.turnPoints[i][0], snakeGame.turnPoints[i][1]);
   }
+  var apples = snakeGame.getApples();
+  for(var i = 0; i < apples.length; ++i) {
+    backCtx.beginPath();
+    var p = sadd(smul(snakeGame.scale, apples[i]), snakeGame.scale/2);
+    backCtx.arc(p[0], p[1], snakeGame.scale/2, 0, 2 * Math.PI, false);
+    backCtx.fillStyle = 'red';
+    backCtx.fill();
+  }
+  backCtx.font="20px Hellki";
+  backCtx.fillText(score,30,30);
 }
 
 /* Launch the game */
